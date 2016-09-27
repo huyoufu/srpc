5c7318f9dca2bef69327eb73174ea3e21e1db14d
huyoufu <huyoufu1988@163.com>
2016-09-27 18:01:51 +0800

    提交测试

---
+++ src/main/java/com/gis09/App.java
@@ -0,0 +1,13 @@
+package com.gis09;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World1!1" );
+    }
+}
